97e68dab817e8a41fe61291a7675ecae353ddeac
deepthikovvuri <deepthireddykovvuri3627@gmail.com>
2026-08-07 10:12:49 +0530

    Add files via upload

---
+++ week 2/build array using permutations task7.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
+
+
+
+
+
+
+
+
+
+output:
+[0,1,2,4,5,3]
