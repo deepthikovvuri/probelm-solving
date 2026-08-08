fc8efb4cd2dc1d8b45810accd762a626d1626fc7
deepthikovvuri <deepthireddykovvuri3627@gmail.com>
2026-08-08 08:54:47 +0530

    Add files via upload

---
+++ week 2/find highest altitude.session 8.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int altitude = 0;
+        int maxAltitude = 0;
+
+        for (int g : gain) {
+            altitude += g;
+            if (altitude > maxAltitude) {
+                maxAltitude = altitude;
+            }
+        }
+
+        return maxAltitude;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+Input
+gain =
+[-5,1,5,0,-7]
+Output
+1
+Expected
+1
