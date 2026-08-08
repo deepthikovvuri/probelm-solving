906b808590eac03da7680494f08fcee3e1aa5ed7
deepthikovvuri <deepthireddykovvuri3627@gmail.com>
2026-08-08 09:02:52 +0530

    Add files via upload

---
+++ week 2/dequeue.session 8.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxUnique = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+
+            deque.addLast(num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            if (deque.size() > m) {
+                int removed = deque.removeFirst();
+
+                map.put(removed, map.get(removed) - 1);
+
+                if (map.get(removed) == 0) {
+                    map.remove(removed);
+                }
+            }
+
+            if (deque.size() == m) {
+                maxUnique = Math.max(maxUnique, map.size());
+            }
+        }
+
+        System.out.println(maxUnique);
+
+        in.close();
+    }
+}
